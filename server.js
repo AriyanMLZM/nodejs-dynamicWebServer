ed71f1cc8bfb9d748ad42cc4e788b2694cc05f98
AriyanMlzm <ryanmolazem@gmail.com>
2024-07-16 13:20:42 +0330

    add contentType

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const http = require('http')
+const path = require('path')
 require('dotenv').config()
+
+const getContentType = require('./utils/getContentType')
 
 const PORT = parseInt(process.env.PORT) || 3500
 
 const server = http.createServer((req, res) => {
   console.log('url: ' + req.url + '\tmethod: ' + req.method)
-  res.end("hello from server")
+
+  const ext = path.extname(req.url)
+  const contentType = getContentType(ext)
+
+  console.log(contentType)
+  
 })
 
 server.listen(PORT, () => console.log(`server is running on PORT ${PORT}`))
---
+++ utils/getContentType.js
@@ -0,0 +1,28 @@
+const getContentType = (ext) => {
+  let contentType = ''
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.jpg':
+      contentType = 'image/jpeg'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.txt':
+      contentType = 'text/plain'
+      break
+    default:
+      contentType = 'text/html'
+  }
+  return contentType
+}
+
+module.exports = getContentType
